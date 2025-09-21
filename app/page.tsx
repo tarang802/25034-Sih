ea5c08c4a360ac242b74e4960d27013258750cc8
Tarang Gupta <taranggupta3108@gmail.com>
2025-09-21 23:10:17 +0530

    Initialized next js latest version setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <h1 className="text-3xl font-bold underline">
-      Hello this is gupta!
+      Hello this is gupta!.lets create sih 
     </h1>
   );
 }
